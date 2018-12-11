6bc6b0999d5bb94d0dfa5aa5374cedd6b8fa8192
vancir <vancirprince@gmail.com>
2018-12-12 00:34:00 +0800

    fixed some compile-time error

---
+++ fugitivec/src/main/java/com/vancir/config/Config.java
@@ -0,0 +1,53 @@
+package com.vancir.config;
+
+import java.io.File;
+
+public class Config {
+	
+	public static final String ORG1 = "org1";
+	public static final String ORG1_MSP = "Org1MSP";
+	public static final String ORG2 = "org2";
+	public static final String ORG2_MSP = "Org2MSP";
+
+	public static final String ADMIN = "admin";
+	public static final String ADMINPW = "adminpw";
+
+	public static final String ORG1_ADMIN_BASE = "crypto-config" + File.separator 
+												+ "peerOrganizations" + File.separator 
+												+ "org1.vancir.com" + File.separator 
+												+ "users" + File.separator 
+												+ "Admin@org1.vancir.com" + File.separator 
+												+ "msp";
+	public static final String ORG1_ADMIN_PK = ORG1_ADMIN_BASE + File.separator + "keystore";
+	public static final String ORG1_ADMIN_CERT = ORG1_ADMIN_BASE + File.separator + "admincerts";
+	public static final String ORG2_ADMIN_BASE = "crypto-config" + File.separator 
+												+ "peerOrganizations" + File.separator 
+                                                + "org2.vancir.com" + File.separator 
+                                                + "users" + File.separator 
+												+ "Admin@org2.vancir.com" + File.separator 
+												+ "msp";
+	public static final String ORG2_ADMIN_PK = ORG2_ADMIN_BASE + File.separator + "keystore";
+	public static final String ORG2_ADMIN_CERT = ORG2_ADMIN_BASE + File.separator + "admincerts";
+
+    public static final String CA_ORG1_URL = "http://localhost:7054";
+	public static final String CA_ORG2_URL = "http://localhost:8054";
+	
+	public static final String ORDERER_NAME = "orderer.vancir.com";
+	public static final String ORDERER_URL = "grpc://localhost:7050";
+	
+	public static final String CHANNEL_NAME = "mychannel";
+	
+	public static final String PEER0_ORG1_NAME = "peer0.org1.vancir.com";
+	public static final String PEER0_ORG1_URL = "grpc://localhost:7051";
+	
+	public static final String PEER1_ORG1_NAME = "peer1.org1.vancir.com";
+	public static final String PEER1_ORG1_URL = "grpc://localhost:7056";
+	
+    public static final String PEER0_ORG2_NAME = "peer0.org2.vancir.com";
+	public static final String PEER0_ORG2_URL = "grpc://localhost:8051";
+	
+	public static final String PEER1_ORG2_NAME = "peer1.org2.vancir.com";
+	public static final String PEER1_ORG2_URL = "grpc://localhost:8056";
+	
+
+}
---
+++ fugitivec/src/main/java/com/vancir/network/CreateChannel.java
@@ -0,0 +1,114 @@
+package com.vancir.network;
+
+import org.apache.log4j.Logger;
+
+import org.hyperledger.fabric.sdk.Enrollment;
+import org.hyperledger.fabric.sdk.security.CryptoSuite;
+import org.hyperledger.fabric_ca.sdk.HFCAClient;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+import javax.xml.bind.DatatypeConverter;
+
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+
+import com.vancir.config.Config;
+import com.vancir.user.AppUser;
+import com.vancir.client.CAClient;
+
+public class CreateChannel {
+
+    private static Logger logger = Logger.getLogger(CreateChannel.class); 
+
+    public static void main(String[] args) throws Exception {
+        AppUser org1Admin = getOrgAdmin(Config.ADMIN, Config.ORG1_MSP,
+                                Config.ORG1_ADMIN_PK, Config.ORG1_ADMIN_CERT);
+        AppUser org2Admin = getOrgAdmin(Config.ADMIN, Config.ORG2_MSP,
+                                Config.ORG2_ADMIN_PK, Config.ORG2_ADMIN_PK);
+    
+        logger.info(org1Admin.toString());
+        logger.info(org2Admin.toString());
+
+
+    }
+    /**
+     * Get organization admin user
+     * 
+     * @param adminName                 Admin user name 
+     * @param msp                       MSP(Membership service provider) name
+     * @param pkFolderPath              The folder path of private key
+     * @param certFolderPath            The folder path of certificate
+     * @return                          A AppUser instance for the specified organization admin user
+     */
+    public static AppUser getOrgAdmin(String adminName, String msp, 
+            String pkFolderPath, String certFolderPath) throws Exception {
+        AppUser orgAdmin = new AppUser();
+        
+        Enrollment enrollOrgAdmin = getEnrollFromPath(pkFolderPath, certFolderPath);
+        orgAdmin.setEnrollment(enrollOrgAdmin);
+        orgAdmin.setMspId(msp);
+        orgAdmin.setName(adminName);
+
+        return orgAdmin;
+    }
+    /**
+     * Get enrollment from the specified 
+     * keystore and admincerts folder path
+     * 
+     * @param pkFolderPath              The folder path of private key
+     * @param certFolderPath            The folder path of certificate
+     * @return                          A Enrollment instance 
+     */
+    public static Enrollment getEnrollFromPath(String pkFolderPath, String certFolderPath) throws Exception {
+        File pkFolder = new File(pkFolderPath);
+        File[] pkFiles = pkFolder.listFiles();
+        String pkName = pkFiles[0].getName();
+
+        File certFolder = new File(certFolderPath);
+        File[] certFile = certFolder.listFiles();
+        String certName = certFile[0].getName();
+
+        PrivateKey key = null;
+        String cert = null;
+        InputStream isKey = null;
+        BufferedReader brKey = null;
+        StringBuilder keyBuilder = new StringBuilder();
+
+        try {
+            isKey = new FileInputStream(pkFolderPath + File.separator + pkName);
+            brKey = new BufferedReader(new InputStreamReader(isKey));
+            
+            for (String line = brKey.readLine(); line != null; line = brKey.readLine()) {
+                if (line.indexOf("PRIVATE") == -1) {
+                    keyBuilder.append(line);
+                }
+            }
+
+            cert = new String(Files.readAllBytes(Paths.get(certFolderPath, certName)));
+            
+            byte[] encodedKey = DatatypeConverter.parseBase64Binary(keyBuilder.toString());
+            PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(encodedKey);
+            KeyFactory kf = KeyFactory.getInstance("ECDSA");
+            key = kf.generatePrivate(keySpec);
+
+        } finally {
+            isKey.close();
+            brKey.close();
+        }
+
+        Enrollment enrollOrgAdmin = new CAEnrollment(key, cert);
+        return enrollOrgAdmin;
+    }
+
+
+    
+}
